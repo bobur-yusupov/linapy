32d35c8f03de3faa9faebffb5ae447f359399127
Bobur Yusupov <86429857+bobur-yusupov@users.noreply.github.com>
2026-03-27 17:29:29 +0100

    Create matrix_mul_1.cu

---
+++ experiments/matrix_mul_1.cu
@@ -0,0 +1,73 @@
+%%writefile matrix_mul.cu
+#include <iostream>
+#include <cuda_runtime.h>
+
+# 1. The Kernel (Executes on the GPU)
+__global__ void matrixMulTiled(float *A, float *B, float *C, int N) {
+  const int TILE_SIZE = 16;
+  __shared__ float tileA[TILE_SIZE][TILE_SIZE];
+  __shared__ float tileB[TILE_SIZE][TILE_SIZE];
+
+  int bx = blockIdx.x; int by = blockIdx.y;
+  int tx = threadIdx.x; int ty = threadIdx.y;
+
+  int row = by * TILE_SIZE + ty;
+  int col = bx * TILE_SIZE + tx;
+
+  float sum = 0.0f;
+
+  for (int i = 0; i < N / TILE_SIZE; ++i) {
+    tileA[ty][tx] = A[row * N + (i * TILE_SIZE + tx)];
+    tileB[ty][tx] = B[(i * TILE_SIZE + ty) * N + col];
+    __syncthreads();
+
+    for (int k = 0; k < TILE_SIZE; ++k) {
+      sum += tileA[ty][k] * tileB[k][tx];
+    }
+    __syncthreads();
+    }
+    C[row * N + col] = sum;
+}
+
+// 2. The Host (Executes on the CPU)
+int main() {
+  int N = 28000; // Matrix Size: 28000 x 28000
+  size_t bytes = N * N * sizeof(float);
+
+  // Allocate memory on the Host (CPU RAM)
+  float *h_A = (float *)malloc(bytes);
+  float *h_B = (float *)malloc(bytes);
+  float *h_C = (float *)malloc(bytes);
+
+  for (int i = 0; i < N * N; i++) {
+    h_A[i] = 1.0f;
+    h_B[i] = 1.0f;
+  }
+
+  float *d_A, *d_B, *d_C;
+  cudaMalloc(&d_A, bytes);
+  cudaMalloc(&d_B, bytes);
+  cudaMalloc(&d_C, bytes);
+
+  cudaMemcpy(d_A, h_A, bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice);
+
+  dim3 threadsPerBlock(16, 16);
+  dim3 blocksPerGrid(N / 16, N / 16);
+
+  matrixMulTiled<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+  cudaMemcpy(h_C, d_C, bytes, cudaMemcpyDeviceToHost);
+
+  std::cout << "Execution complete. Top-left value is: " << h_C[0] << std::endl;
+
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+
+  free(h_A);
+  free(h_B);
+  free(h_C);
+
+  return 0;
+}
